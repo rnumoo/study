04f67157907e3b22cb26e3c106e86d5971ae1b86
rnumoo <88758366+rnumoo@users.noreply.github.com>
2021-09-14 09:36:56 +0900

    인터셉터 변경

--- src/main/java/study/notice/interceptor/CommonInterceptor.java
+++ src/main/java/study/notice/interceptor/CommonInterceptor.java
@@ -14,13 +14,25 @@ public class CommonInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
 		
+		
+		
 		//인증된사용자 여부
 		boolean isAuthenticated = false;
-		
+		/**
 		if (RequestContextHolder.getRequestAttributes() == null) {
 			isAuthenticated = false;
 		} else {
 			if (RequestContextHolder.getRequestAttributes().getAttribute("userLoginVO", RequestAttributes.SCOPE_SESSION) == null) {
+				isAuthenticated = false;
+			} else {
+				isAuthenticated = true;
+			}
+		}
+		**/
+		if(request.getSession() == null) {
+			isAuthenticated = false;
+		} else {
+			if(request.getSession().getAttribute("userLoginVO") == null) {
 				isAuthenticated = false;
 			} else {
 				isAuthenticated = true;
